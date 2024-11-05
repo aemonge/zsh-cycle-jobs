5340c17b6d639fc80c557f30aaba238ce70159e1
aemonge <andres@aemonge.com>
2024-11-05 17:08:27 +0100

    fix(load): Fixing the loading of pling

--- zsh-cycle-jobs.plugin.zsh
+++ zsh-cycle-jobs.plugin.zsh
@@ -5,7 +5,7 @@
 # Version: 0.0.1
 
 # Add functions directory to fpath
-fpath="${0:h}/functions${fpath:+:${fpath}}"
+fpath=( "${0:h}/functions" $fpath )
 
 # Autoload the main function
 autoload -Uz zsh_cycle_jobs_cycle
@@ -16,27 +16,18 @@ FANCY_ZSH_LAST_JOB_NUMBER="/tmp/zsh_cycle_jobs__last_job_number"
 # Get the next jobs as array
 get_next_job() {
     local all_jobs=($(jobs | awk -F'[][]' '{print $2}' | tr -d ' '))
-    # Check if the array is empty
     if [ ${#all_jobs[@]} -eq 0 ]; then
         return
     fi
 
-    # Get the current index, default to 1 if not provided
     local current_index=$1
     if [ -z "$current_index" ]; then
         current_index=1
     fi
 
-    # Calculate next job number maintaining 1-based indexing
     local array_size=${#all_jobs[@]}
-    
-    # Apply modulo for circular rotation
     local rotated_index=$((current_index % array_size))
-    
-    # Add 1 to convert back to 1-based job numbers
     local next_job_index=$((rotated_index + 1))
-    
-    # Return the job number (converting to 0-based for array access)
     echo -n "${all_jobs[$next_job_index]}"
 }
 
