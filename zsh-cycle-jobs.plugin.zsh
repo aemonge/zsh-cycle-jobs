e28d22a4f4e023acd62ffe7ca9a2469454ab5dbe
aemonge <andres@aemonge.com>
2025-08-12 11:31:31 +0200

    feat(fzf): Use fzf as a list chooser

--- zsh-cycle-jobs.plugin.zsh
+++ zsh-cycle-jobs.plugin.zsh
@@ -1,36 +1,56 @@
 #!/usr/bin/env zsh
 
-# ZSH Cycle Jobs Plugin
+# ZSH FZF Job Chooser Plugin
 # Author: aemonge
-# Version: 0.0.1
+# Version: 0.0.2
 
-# Add functions directory to fpath
-fpath=( "${0:h}/functions" $fpath )
-
-# Autoload the main function
-autoload -Uz zsh_cycle_jobs_cycle
-
-# Job tracking file
-FANCY_ZSH_LAST_JOB_NUMBER="/tmp/zsh_cycle_jobs__last_job_number"
-
-# Get the next jobs as array
-get_next_job() {
-    local all_jobs=($(jobs | awk -F'[][]' '{print $2}' | tr -d ' '))
-    if [ ${#all_jobs[@]} -eq 0 ]; then
-        return
+# Function to choose a job with fzf and foreground it
+_fzf_job_chooser() {
+    # Check if fzf is installed
+    if ! command -v fzf >/dev/null; then
+        zle -M "fzf not found. Please install fzf."
+        return 1
     fi
 
-    local current_index=$1
-    if [ -z "$current_index" ]; then
-        current_index=1
+    local jobs_list
+    jobs_list=$(jobs -l)
+
+    if [[ -z "$jobs_list" ]]; then
+        zle -M "No jobs found"
+        return 1
     fi
 
-    local array_size=${#all_jobs[@]}
-    local rotated_index=$((current_index % array_size))
-    local next_job_index=$((rotated_index + 1))
-    echo -n "${all_jobs[$next_job_index]}"
+    # Parse and format jobs for fzf
+    local job_lines
+    job_lines=(${(f)"$(jobs -l | awk '{
+        job_num = substr($1, 2, length($1)-2);
+        state = $3;
+        cmd = "";
+        for (i=4; i<=NF; i++) cmd = cmd $i " ";
+        printf "[%s] %s %s\n", job_num, state, cmd;
+    }')"})
+
+    # Use fzf to select a job
+    local selected
+    selected=$(printf "%s\n" "${job_lines[@]}" | fzf --prompt="Select job: ")
+
+    if [[ -z "$selected" ]]; then
+        return 1
+    fi
+
+    # Extract job number from selection
+    local job_id
+    job_id=$(echo "$selected" | awk 'match($0, /\[([0-9]+)\]/, arr) { print arr[1] }')
+
+    if [[ -z "$job_id" ]]; then
+        return 1
+    fi
+
+    # Bring selected job to foreground
+    BUFFER="fg %${job_id}"
+    zle accept-line
 }
 
-# Bind the Ctrl+Z shortcut to the zsh_cycle_jobs_cycle function
-zle -N zsh_cycle_jobs_cycle
-bindkey '^Z' zsh_cycle_jobs_cycle
+# Create ZLE widget and bind to Ctrl+J
+zle -N _fzf_job_chooser
+bindkey '^Z' _fzf_job_chooser
